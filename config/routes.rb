b6ee9e8c9d5558b643d6eaa6003ea23d388ff028
Ankit Goyal <ankitgoyal100@gmail.com>
2013-07-11 19:12:32 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
-   match '/about',   to: 'static_pages#about',   via: 'get'
-   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 root  'static_pages#home'  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ankit Goyal"
+    email    "ankitgoyal100@gmail.com"
+    password "ferrari"
+    password_confirmation "ferrari"
+  end
+end
